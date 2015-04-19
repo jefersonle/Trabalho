484dbea82fa1b9294c113e56ee024b1c2f68b783
jefersonle <jefersonle@users.noreply.github.com>
2015-04-19 15:09:19 -0300

    Update cadastrar.php

--- cor/cadastrar.php
+++ cor/cadastrar.php
@@ -1,3 +1,27 @@
 <?php
-	
+//Incluindo arquivo de conexao com DB
+include '../inc/conexao.inc.php';
+//Testando requisição
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	$nome = $_POST['nome'];
+	if(!empty($nome)){
+		$sql = "INSERT INTO cor (nome) VALUES ('$nome')";
+		pg_query($sql);
+		header('Location: listar.php');
+	}else{
+		echo 'Nome invalido!';
+	}
+}
 ?>
+<html>
+	<head>
+		<title>Cadastrar Cor</title>
+	</head>
+	<body>
+		<h2>Cadastrar Cor</h2>
+		<form action="" method="post">
+			<p>Nome:<input type="text" name="cor" /></p>
+			<p><input type="submit" value="Cadastrar Cor"></p>
+		</form>
+	</body>
+</html>
