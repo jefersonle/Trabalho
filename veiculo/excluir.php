4788a079798e7af3a29297a042bc3a891413fab4
jefersonle <jefersonle@users.noreply.github.com>
2015-04-19 14:49:41 -0300

    Update excluir.php

--- veiculo/excluir.php
+++ veiculo/excluir.php
@@ -1,3 +1,10 @@
 <?php
-	
+//Inclui arquivo de conexão ao BD
+include '../inc/conexao.inc.php';
+//Pega o id do cadastro para deletar no BD
+if(isset($_GET['id'])){
+	$id = $_GET['id'];
+	$sql = "DELETE FROM cor where id_cor='$id";
+	pg_query($sql);
+}
 ?>
