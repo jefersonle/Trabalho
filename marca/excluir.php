1155c1af327aac20353993bbfafcf084eb61b9e1
jefersonle <jefersonle@users.noreply.github.com>
2015-04-19 14:47:56 -0300

    Update excluir.php

--- marca/excluir.php
+++ marca/excluir.php
@@ -1,3 +1,12 @@
 <?php
-	
+<?php
+//Inclui arquivo de conexão ao BD
+include '../inc/conexao.inc.php';
+//Pega o id do cadastro para deletar no BD
+if(isset($_GET['id'])){
+	$id = $_GET['id'];
+	$sql = "DELETE FROM marca where id_marca='$id";
+	pg_query($sql);
+}
+?>	
 ?>
