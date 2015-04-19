8f697978861361c77c319a93fd0af73fd2cd0695
jefersonle <jefersonle@users.noreply.github.com>
2015-04-19 14:38:10 -0300

    Update listar.php

--- veiculo/listar.php
+++ veiculo/listar.php
@@ -1,3 +1,47 @@
 <?php
 	
 ?>
+<html>
+	<head>
+		<title>Listagem de Veiculos</title>
+	</head>
+	<body>
+		<div>
+			<h2>Listagem de Veiculos</h2>
+			<table>
+				<tr>
+					<td>
+						ID
+					</td>
+					<td>
+						Nome
+					</td>
+					<td>
+						ID do Modelo
+					</td>
+					<td>
+						Modelo
+					</td>
+					<td>
+						ID da Cor
+					</td>
+					<td>
+						Cor
+					</td>
+					<td>
+						Placa
+					</td>
+					<td>
+						Ano de Fabricação
+					</td>
+					<td>
+						Ano do Modelo
+					</td>
+					<td>
+						Preço
+					</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
