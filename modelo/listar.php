14486e685baa47c1f0fb3487d8e538c0a4d351b6
jefersonle <jefersonle@users.noreply.github.com>
2015-04-19 14:33:49 -0300

    Update listar.php

--- modelo/listar.php
+++ modelo/listar.php
@@ -1,3 +1,26 @@
 <?php
 	
 ?>
+<html>
+	<head>
+		<title>Listagem de Modelos</title>
+	</head>
+	<body>
+		<div>
+			<h2>Listagem de Marcas</h2>
+			<table>
+				<tr>
+					<td>
+						ID
+					</td>
+					<td>
+						ID da Marca
+					</td>
+					<td>
+						Nome
+					</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
