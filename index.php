218e08e64029e2ec1a7060c1fbe3c439c6e8e7a1
jefersonle <jefersonle@users.noreply.github.com>
2015-04-19 14:23:24 -0300

    Update
    Criado arquivo da pagina inicial

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<?php
-	
-?>
+<html>
+	<head>
+		<title>Cadastro de Veículos</title>
+	</head>
+	<body>
+		<div>
+			<h2>Cadastro de Veículos</h2>
+			<table>
+				<tr>
+					<td><a href="veiculo/listar.php">Gerenciar Veículos</a></td>
+				</tr>
+				<tr>
+					<td><a href="modelo/listar.php">Gerenciar Modelos</a></td>
+				</tr>
+				<tr>
+					<td><a href="vmarca/listar.php">Gerenciar Marcas</a></td>
+				</tr>
+				<tr>
+					<td><a href="cor/listar.php">Gerenciar Cores</a></td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
