1382799d4ad40963cf4e2de094389a2e210fa639
jefersonle <jefersonle@users.noreply.github.com>
2015-04-19 14:31:58 -0300

    Update
    HTML Inicial

--- marca/listar.php
+++ marca/listar.php
@@ -1,3 +1,23 @@
 <?php
 	
 ?>
+<html>
+	<head>
+		<title>Listagem de Marcas</title>
+	</head>
+	<body>
+		<div>
+			<h2>Listagem de Marcas</h2>
+			<table>
+				<tr>
+					<td>
+						ID
+					</td>
+					<td>
+						Nome
+					</td>
+				</tr>
+			</table>
+		</div>
+	</body>
+</html>
