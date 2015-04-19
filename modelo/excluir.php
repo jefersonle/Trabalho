0a447912c794376d425653a8dac6a43f56ea43aa
jefersonle <jefersonle@users.noreply.github.com>
2015-04-19 15:01:35 -0300

    Update excluir.php

--- modelo/excluir.php
+++ modelo/excluir.php
@@ -1,7 +1,7 @@
 <?php
 //Inclui arquivo de conexão ao BD
 include '../inc/conexao.inc.php';
-//Pega o id do cadastro para deletar no BD
+//Pega o id do cadastro e deleta do BD
 if(isset($_GET['id'])){
 	$id = $_GET['id'];
 	$sql = "DELETE FROM modelo where id_modelo='$id";
