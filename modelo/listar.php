24b164a9dd31303a9bed57deb558a185b9d95f92
jefersonle <jefersonle@users.noreply.github.com>
2015-04-21 13:41:51 -0300

    Update listar.php

--- modelo/listar.php
+++ modelo/listar.php
@@ -1,5 +1,14 @@
 <?php
-	
+include 'conexao.inc.php';
+$sql = "SELECT
+		modelo.id_modelo,
+		modelo.id_marca,
+		modelo.nome modelo_nome,
+		marca.nome marca_nome
+	FROM
+		modelo
+	INNER JOIN marca ON marca.id_marca = modelo.id_modelo";
+$query = pg_query($sql);
 ?>
 <html>
 	<head>
@@ -14,13 +23,13 @@
 						ID
 					</td>
 					<td>
-						ID da Marca
-					</td>
-					<td>
 						Nome
 					</td>
 					<td>
-						Ver
+						ID da Marca
+					</td>
+					<td>
+						Marca
 					</td>
 					<td>
 						Editar
@@ -29,6 +38,37 @@
 						Deletar
 					</td>
 				</tr>
+				<?php
+					while($dados = pg_fetch_assoc($query)){
+				?>	
+				
+				<tr>
+					<td>
+						<?=$dados['id_modelo']?>
+					</td>
+					<td>
+						<?=$dados['modelo_nome']?>
+					</td>
+					<td>
+						<?=$dados['id_marca']?>
+					</td>
+					<td>
+						<?=$dados['marca_nome']?>
+					</td>
+					<td>
+						<a href="editar.php?id=<?=$dados['id_modelo']?>">Editar</a>
+					</td>
+					<td>
+						<a href="excluir.php?id=<?=$dados['id_modelo']?>">Excluir</a>
+					</td>
+				</tr>
+						
+				<?php		
+					}
+				?>
+				
+				
+				
 			</table>
 		</div>
 	</body>
