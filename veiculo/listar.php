4808b98565c04f3f48007b66b8ab9d7f3ab65fb6
jefersonle <jefersonle@users.noreply.github.com>
2015-04-21 13:54:49 -0300

    Update listar.php

--- veiculo/listar.php
+++ veiculo/listar.php
@@ -1,5 +1,27 @@
 <?php
+include 'conexao.inc.php';
+
+$sql = "SELECT
+		veiculo.id_veiculo,
+		veiculo.nome nome_veiculo,
+		veiculo.id_modelo,
+		veiculo.id_cor,
+		veiculo.placa,
+		veiculo.ano_fab,
+		veiculo.ano_mod,
+		veiculo.preco,
+		modelo.nome nome_modelo,
+		modelo.id_marca,
+		cor.nome nome_cor,
+		marca.nome nome_marca
+	FROM
+		veiculo
+	INNER JOIN modelo ON modelo.id_modelo = veiculo.id_modelo
+	INNER JOIN cor ON cor.id_cor = veiculo.id_cor
+	INNER JOIN marca ON marca.id_marca = modelo.id_marca";
 	
+$query = pg_query($sql);
+
 ?>
 <html>
 	<head>
@@ -23,6 +45,12 @@
 						Modelo
 					</td>
 					<td>
+						ID da Marca
+					</td>
+					<td>
+						Marca
+					</td>
+					<td>
 						ID da Cor
 					</td>
 					<td>
@@ -41,9 +69,6 @@
 						Preço
 					</td>
 					<td>
-						Ver
-					</td>
-					<td>
 						Editar
 					</td>
 					<td>
@@ -51,6 +76,62 @@
 					</td>
 					
 				</tr>
+				
+				<?php
+					while($dados = pg_fetch_assoc($query)){
+				?>		
+				
+				<tr>
+					<td>
+						<?=$dados['id_veiculo']?>
+					</td>
+					<td>
+						<?=$dados['nome_veiculo']?>
+					</td>
+					<td>
+						<?=$dados['id_modelo']?>
+					</td>
+					<td>
+						<?=$dados['nome_modelo']?>
+					</td>
+					<td>
+						<?=$dados['id_marca']?>
+					</td>
+					<td>
+						<?=$dados['nome_marca']?>
+					</td>
+					<td>
+						<?=$dados['id_cor']?>
+					</td>
+					<td>
+						<?=$dados['nome_cor']?>
+					</td>
+					<td>
+						<?=$dados['placa']?>
+					</td>
+					<td>
+						<?=$dados['ano_fab']?>
+					</td>
+					<td>
+						<?=$dados['ano_mod']?>
+					</td>
+					<td>
+						<?=$dados['preco']?>
+					</td>
+					<td>
+						<a href="editar.php?id=<?=$dados['id_veiculo']?>">Editar</a>
+					</td>
+					<td>
+						<a href="excluir.php?id=<?=$dados['id_veiculo']?>">Excluir</a>
+					</td>
+					
+				</tr>		
+						
+				<?php		
+					}
+				?>
+				
+				
 			</table>
 		</div>
 	</body>
