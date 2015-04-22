561723d428b8bc0c5ccb76079c254d685238896d
jefersonle <jefersonle@users.noreply.github.com>
2015-04-22 18:15:55 -0300

    Update editar.php

--- veiculo/editar.php
+++ veiculo/editar.php
@@ -1,7 +1,7 @@
 <?php
 //Incluindo arquivo de conexao com DB
 include '../inc/conexao.inc.php';
-
+//Pegando item a ser editado por GET
 $id = $_GET['id'];
 //Testando requisição
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
@@ -12,6 +12,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$ano_mod = $_POST['ano_mod'];
 	$preco = $_POST['preco'];
 	if(!empty($modelo) && !empty($cor) && !empty($placa) && !empty($ano_fab) && !empty($ano_mod)){
+		//Modifica informações do item especificado por GET
 		$sql = "UPDATE veiculo SET id_modelo='$modelo', id_cor='$cor', placa='$placa', ano_fab='$ano_fab', ano_mod='$ano_mod', preco='$preco' WHERE id_veiculo='$id'";
 		$query = pg_query($sql);
 		
@@ -31,13 +32,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 				Selecione o modelo do veiculo: 
 				<select name="modelo">
 				<?php 
+					//Seleciona todas as informações do item passado por GET para editar no form
 					$sql = "SELECT * FROM veiculo WHERE id_veiculo='$id'";
 					$query = pg_query($sql);
 					$veiculo = pg_fetch_assoc($query);
 					
-					
+					//Seleciona todos os itens da tabela modelo para criar um select de modelos
 					$sql = "SELECT * FROM modelo";
 					$modelos = pg_query($sql);
+					//Laço para criar select de modelos
 					while($dados = pg_fetch_assoc($modelos)){
 				?>
 					<option value="<?=$dados['id_modelo']?>" ><?=$dados['nome']?></option>
@@ -50,8 +53,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 				Selecione a Cor do Veiculo: 
 				<select name="cor">
 				<?php 
+					//Seleciona todos os itens da tabela cor para criar select de cores
 					$sql = "SELECT * FROM cor";
 					$cores = pg_query($sql);
+					//Laço para criar selecte de cores
 					while($dados = pg_fetch_assoc($cores)){
 				?>
 					<option value="<?=$dados['id_cor']?>"><?=$dados['nome']?></option>
