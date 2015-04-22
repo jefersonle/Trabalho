1c03fc68e0de74bdc8e312cd00e546c9cd138022
jefersonle <jefersonle@users.noreply.github.com>
2015-04-22 17:52:10 -0300

    Update conexao.inc.php

--- inc/conexao.inc.php
+++ inc/conexao.inc.php
@@ -1,8 +1,10 @@
 <?php
+//Variaveis utilizadas na conexao
 $host = 'localhost';
 $usuario = 'postgres';
 $senha = '123';
 $banco = 'veiculos';
 
+//conecta ao banco de dados
 pg_connect("host=$host user=$usuario password=$senha dbname=$banco");
 ?>
