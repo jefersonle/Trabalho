5c8961fa3c19ef0bba1b412f7bdc402eda716f27
jefersonle <jefersonle@users.noreply.github.com>
2015-04-23 20:02:32 -0300

    Update excluir.php

--- modelo/excluir.php
+++ modelo/excluir.php
@@ -10,6 +10,9 @@ include '../inc/conexao.inc.php';
 //Pega o id do cadastro e deleta do BD
 if(isset($_GET['id'])){
 	$id = $_GET['id'];
+	$sql = "DELETE FROM veiculo WHERE id_modelo = '$id'";
+	pg_query($sql);
+	
 	$sql = "DELETE FROM modelo where id_modelo='$id'";
 	pg_query($sql);
 }
